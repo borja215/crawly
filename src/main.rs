d043cba8947ded8add067cd6e1b40cb6d49efb35
Borja De La Viuda Herrero <borjadlv81@gmail.com>
2026-04-05 20:10:41 +0900

    feat: use args for inital url

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ pub mod parser;
 use crate::crawler::Crawler;
 use reqwest::Client;
 use url::Url;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    url: String
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let initial_url = Url::parse("https://crawler-test.com/");
+    let args = Cli::parse();
+    let initial_url = Url::parse(args.url.as_str());
+    
     match initial_url {
         Ok(url) => {
             let mut crawler = Crawler::new(url, Client::new());
